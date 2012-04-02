2ac3c9e729f25e6b81ce6c3759733f07da2d709b
Changok Kim <manyreal@gmail.com>
2012-04-02 00:12:38 -0400

    bio.py: change the overall code to be expendable parsed_*: name of all parsed result file will be named as parsed_blahblah.txt parser.sh: also modified to expandable, require two program argument always as input and output filename

--- bio.py
+++ bio.py
@@ -1,28 +1,37 @@
+import sys
 #glofish
 
-f = open("parsedRank.txt", "r")
-lines = f.readlines()
-WIDTH = 1000000
-MAX_CHRPOS = 0 
-num_mutant = 8
+filename = sys.argv[1]
+print "GloshFish chart"
+print "Read a file : " + filename
 
+f = open(filename, "r")
+lines = f.readlines()
+WIDTH = 1000000 # width(range) of bar(column)
+MAX_CHRPOS = 0	# maximum number of ChrPos : generate reasonable number of bars(columns) of chart
+NUM_MUTANT = 0	# the number of Mutants
+THRESHOLD = 300	# threshold for pretty chart printer
+
+# first pass: pick up the number of mutant in the input data and the max of ChrPos
 for i in lines:
+	NUM_MUTANT = max(int(i.split(" ")[0]), NUM_MUTANT)
 	MAX_CHRPOS = max(int(i.split(" ")[1]), MAX_CHRPOS)
 
-print MAX_CHRPOS
+print "Maximum ChrPos : " + str(MAX_CHRPOS)
 
-num_bar = MAX_CHRPOS / WIDTH
+NUM_BARS = MAX_CHRPOS / WIDTH	# the number of bars(column) in chart
 
-# initialzing part
+# initialzing table part
 mtable = []
-totals = []
+totals = []	# totals of chr for calculating portion of each bar
 
-for i in range(num_mutant):
+for i in range(NUM_MUTANT):
 	mtable.append([])
 	totals.append(0)
-	for j in range(num_bar+1):
+	for j in range(NUM_BARS+1):
 		mtable[i].append(0)
 
+# second pass: increase the count of each ChrPos of each Mutant
 for i in lines:
 	row = i.split(" ")
 	Chr = int(row[0])-1
@@ -30,14 +39,18 @@ for i in lines:
 	mtable[Chr][index] += 1
 	totals[Chr] += 1
 
-for i in range(num_mutant):
+# pretty printout part
+for i in range(NUM_MUTANT):
+	compress = totals[i]/THRESHOLD +1	# for pretty "bar" printout
 	print "MUTANT TABLE : mutant_%s" % (i+1),
 	print " / total: " + str(totals[i])
+	if compress > 1:
+		print "The length of each bar is compressed(divided) by " + str(compress)
 	print mtable[i]
-	for j in range(num_bar+1):
+	for j in range(NUM_BARS+1):
 		portion = (float(mtable[i][j])/float(totals[i]))*100.0
 		portion = round( portion, 2)
 		print str(mtable[i][j]) + "(" + str(portion) + "%):\t",
-		for h in range(mtable[i][j]):
+		for h in range(mtable[i][j]/compress):
 			print "|",
 		print ""
--- parser.sh
+++ parser.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cat interRank.txt | sed '/^"mut/ d' | sed 's/"[0-9]*" \("mutant_[1-8]"\) .* \([0-9]*\) ".*" .*/\1 \2/' | sed 's/"//g' | sed 's/mutant_//g' > parsedRank.txt
+cat $1 | sed '/^""/ d' | sed 's/"[0-9]*" \("mutant_[1-9]*"\) .* \([0-9]*\) ".*" .*/\1 \2/' | sed 's/"//g' | sed 's/mutant_//g' > $2
+#cat $1 | sed '/^"mut/ d' | sed 's/"[0-9]*" \("mutant_[1-8]"\) .* \([0-9]*\) ".*" .*/\1 \2/' | sed 's/"//g' | sed 's/mutant_//g' > parsedRank.txt
