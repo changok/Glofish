c7a2ea370065be1210b9ddb84b69e9e55004878e
Changok Kim <manyreal@gmail.com>
2012-04-07 01:06:40 -0400

    Modified program This version of program can differentiate Chr section in each mutant, otherwise previous one does not. Overall strategy drawing on the brute force chart has not been changed.

--- bio.py
+++ bio.py
@@ -8,49 +8,72 @@ print "Read a file : " + FILENAME
 
 f = open(FILENAME, "r")
 lines = f.readlines()
-MAX_CHRPOS = 0	# maximum number of ChrPos : generate reasonable number of bars(columns) of chart
+MAX_CHRPOS = 0	# maximum number of rChrPos : generate reasonable number of bars(columns) of chart
 NUM_MUTANT = 0	# the number of Mutants
 THRESHOLD = 300	# threshold for pretty chart printer
+ROMANS = ["I", "II", "III", "IV", "V", "X"]
 
-# first pass: pick up the number of mutant in the input data and the max of ChrPos
+# first pass: pick up the number of mutant in the input data and the max of rChrPos
 for i in lines:
 	NUM_MUTANT = max(int(i.split(" ")[0]), NUM_MUTANT)
-	MAX_CHRPOS = max(int(i.split(" ")[1]), MAX_CHRPOS)
+	MAX_CHRPOS = max(int(i.split(" ")[2]), MAX_CHRPOS)
 
-print "Maximum ChrPos : " + str(MAX_CHRPOS)
+print "Maximum rChrPos : " + str(MAX_CHRPOS)
 
 NUM_BARS = MAX_CHRPOS / WIDTH	# the number of bars(column) in chart
 
+
 # initialzing table part
-mtable = []
+mutants = []
+Chrs = []
 totals = []	# totals of chr for calculating portion of each bar
 
-for i in range(NUM_MUTANT):
-	mtable.append([])
-	totals.append(0)
-	for j in range(NUM_BARS+1):
-		mtable[i].append(0)
 
-# second pass: increase the count of each ChrPos of each Mutant
+# check if there are all Chr exist in each mutant_id
+for i in range(NUM_MUTANT):
+	mutants.append([])	# appending each Chr array to each mutant
+	totals.append([])
+	for j in range(6):
+		mutants[i].append([])	# appending each rChr array to each Chr
+		totals[i].append(0)
+		for k in range(NUM_BARS+1):
+			mutants[i][j].append(0)
+
+# second pass : increase the count of each ChrPos of each Mutant
 for i in lines:
 	row = i.split(" ")
-	Chr = int(row[0])-1
-	index = int(row[1])/WIDTH
-	mtable[Chr][index] += 1
-	totals[Chr] += 1
+	mutant = int(row[0])-1
+	index = int(row[2])/WIDTH
+	chrnum = -1
+	if row[1] == "I": chrnum = 0
+	elif row[1] == "II": chrnum = 1
+	elif row[1] == "III": chrnum = 2
+	elif row[1] == "IV": chrnum = 3
+	elif row[1] == "V": chrnum = 4
+	elif row[1] == "X": chrnum = 5
+	
+	if chrnum == -1:
+		print "unexpected Chr error"
+		sys.exit(0)
+	mutants[mutant][chrnum][index] += 1
+	totals[mutant][chrnum] += 1
 
 # pretty printout part
 for i in range(NUM_MUTANT):
-	compress = totals[i]/THRESHOLD +1	# for pretty "bar" printout
-	print "MUTANT TABLE : mutant_%s" % (i+1),
-	print " / total: " + str(totals[i])
-	if compress > 1:
-		print "The length(height) of each bar is compressed(divided) by " + str(compress) + "."
-	print "The range of ChrPos on each bar is " + str(WIDTH) + "(base)."
-	for j in range(NUM_BARS+1):
-		portion = (float(mtable[i][j])/float(totals[i]))*100.0
-		portion = round( portion, 2)
-		print str(j+1) + ": "  + str(mtable[i][j]) + "(" + str(portion) + "%):\t",
-		for h in range(mtable[i][j]/compress):
-			print "|",
+	print "MUTANT TABLE : mutant_%s" % (i+1)
+	for j in range(6):
+		compress = totals[i][j]/THRESHOLD +1	# for pretty "bar" printout
+		print "Section : %s (Chr)" % ROMANS[j],
+		print "	/ total : " + str(totals[i][j])
+		if compress > 1:
+			print "The length(height) of each bar is compressed(divided) by " + str(compress) + "."
+		print "The range of ChrPos on each bar is " + str(WIDTH) + "(base)."
+		#print mutants[i][j]
+		for k in range(NUM_BARS+1):
+			portion = (float(mutants[i][j][k])/float(totals[i][j]))*100.0
+			portion = round( portion, 2)
+			print str(k+1) + ": " + str(mutants[i][j][k]) + "(" + str(portion) + "%):\t",
+			for h in range(mutants[i][j][k]/compress):
+				print "|",
+			print ""
 		print ""
--- parser.sh
+++ parser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat $1 | sed '/^""/ d' | sed 's/"[0-9]*" \("mutant_[1-9]*"\) .* \([0-9]*\) ".*" .*/\1 \2/' | sed 's/"//g' | sed 's/mutant_//g' > $2
+cat $1 | sed '/^"mutantID"/ d' | sed '/^""/ d' | sed 's/"[0-9]*" \("mutant_[1-9]*"\) \("[I,V,X]*"\) .* \([0-9]*\) ".*" .*/\1 \2 \3/' | sed 's/"//g' | sed 's/mutant_//g' > $2
 #cat $1 | sed '/^"mut/ d' | sed 's/"[0-9]*" \("mutant_[1-8]"\) .* \([0-9]*\) ".*" .*/\1 \2/' | sed 's/"//g' | sed 's/mutant_//g' > parsedRank.txt
